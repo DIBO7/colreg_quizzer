5b9f577dc2d8af757a9c4b445b08db295294e130
Ibrahim Oduola <ibrahim.oduola007@gmail.com>
2022-12-17 18:14:28 -0800

    css may still need some troubleshooting afterall **MAY

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
+import "../styles/index.css";
 import '../styles/globals.css';
 import "../styles/generic.css";
-import "../styles/index.css"
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
